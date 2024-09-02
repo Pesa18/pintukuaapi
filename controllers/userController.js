913128e60d4303ab0d76289ecf2608279478a44c
DESKTOP-H11A5IA\dexter18 <asepalamin42@gmail.com>
2024-09-02 15:31:47 +0700

    gas

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,9 @@
-const user = require("../models/userModel");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 exports.getAllUsers = async (req, res) => {
-  await user.getAll((err, users) => {
-    try {
-      if (err) res.status(500).send(err);
-      res.json(users);
-      console.log(users);
-    } catch (err) {
-      console.error(err.message);
-    }
-  });
+  const users = await prisma.users.findMany();
+  res.json(users);
+  console.log(users);
 };
